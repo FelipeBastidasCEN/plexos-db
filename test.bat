6e83e79647a5ad37000c06a72cec1c5499dba215
Felipe Bastidas <felipe.bastidas@coordinador.cl>
2026-01-16 09:27:23 -0300

    UPDATE: test case

--- test.bat
+++ test.bat
@@ -1,6 +1,4 @@
 @echo off
 
-echo Iniciando TEST REAL
 uv run --no-cache plexos-db import --input "C:\Users\felipe.bastidas\test\PCP\20251011\Datos\Model PRGdia_Full_Definitivo Solution\Model PRGdia_Full_Definitivo Solution.zip" --output "C:\Users\felipe.bastidas\test\PCP\20251011\Datos\Model PRGdia_Full_Definitivo Solution\pcp.duckdb"
 
-echo TEST FINALIZADO
