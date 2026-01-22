1e4faff3f05a09e87a5e722afd37ab77f73b3536
FasoCoding <bastidas.felipe@gmail.com>
2026-01-21 21:25:18 -0300

    FIX: TableRegister TypeHints
    Ok, esto esta raro, pero no voy a ponerle mucho dado que estoy con Ty
    que esta en beta jajaa.. puede ser bug.

--- src/plexos_db/model/entities/table_register.py
+++ src/plexos_db/model/entities/table_register.py
@@ -1,17 +1,19 @@
-from typing import NamedTuple
-
+from typing import Any
 from .t_object import TObject2
 from .t_key import TKey
 
 
 class TableRegister:
-    SUPPORTED_TABLES: dict[str, NamedTuple] = {
+    SUPPORTED_TABLES: dict[str, Any] = {
         "t_object": TObject2,
         "t_key": TKey,
     }
 
+    def __init__(self, schema: str):
+        self.schema = schema
+
     @classmethod
-    def get_table(cls, table_name: str) -> NamedTuple:
+    def get_table(cls, table_name: str) -> Any | None:
         if not cls.is_supported(table_name):
             return None
         return cls.SUPPORTED_TABLES[table_name]
